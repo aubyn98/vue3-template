d269369d2ba53d781c0a5432c3ef9ffaac314d82
aubyn <aubyn@vip.qq.com>
2021-11-23 18:21:32 +0800

    change utils

---
+++ src/common/utils/EventBus/index.d.ts
@@ -0,0 +1,8 @@
+export declare class EventBus {
+  constructor()
+  on(event: string, listener: (...args: Array<any>) => void): () => void
+  off(event: string, listener: (...args: Array<any>) => void): void
+  emit(event: string, ...args: Array<any>): void
+  once(event: string, listener: (...args: Array<any>) => void): void
+}
+export default EventBus
